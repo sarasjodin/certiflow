6da377e46c294380d1aa3a6aabaf68c0c9118b5b
La svedese <5554930+sarasjodin@users.noreply.github.com>
2026-05-18 00:20:19 +0200

    Implement global error handling (#72)

--- CertiFlowApp/Program.cs
+++ CertiFlowApp/Program.cs
@@ -35,6 +35,7 @@ logger.LogInformation(
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseStatusCodePages();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
